05155a10bcfd801198de10400fc520eb7f3bf6ef
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2020-01-03 14:49:31 -0500

    Hice una funcion que trabaje matrices

--- fun.py
+++ fun.py
@@ -1,5 +1,19 @@
-#https://www.youtube.com/watch?v=hTLOjEfRghI
- import numpy as np
- B=np.matrix([1,2],[3,4],[5,6])
+# Creando una funcion que revisa las matrices
 
- print(B)
+import numpy as np
+
+B = np.matrix([[1, 2], [3, 4]])
+
+
+def mult(A):
+    M = np.matrix(np.zeros((2, 2)))
+    for i in range(0, M.shape[0]):
+        for j in range(0, M.shape[1]):
+            M[i, j] = A[i, j]*2
+    return(M)
+
+
+renglones = B.shape[0]
+columnas = B.shape[1]
+
+print(mult(B))
